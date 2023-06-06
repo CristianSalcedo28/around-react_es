2fc8729f479376fd8e2c841e1844a62e0f8f5488
Cristian David Salcedo <cristian.salcedo@outlook.com>
2023-06-06 00:23:14 -0300

    withCard

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Card({name, link, likes, handleCardClick}) {
+
+  return (
+      <div className="cards__item">
+        <img 
+          onClick={() => handleCardClick({name, link})}
+          src={link}
+          alt=""
+          className="cards__image"
+        />
+        <button type="button" className="button-trash"></button>
+        <div className="cards__text">
+            <h4 className="cards__name">{name}</h4>
+            <div className="cards__like">
+              <button type="button" className="button-like"></button>
+              <span className="likes__counter">{likes}</span>
+            </div>
+        </div>
+      </div>
+)
+}
+
+export default Card;
+
