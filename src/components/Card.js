15018217f7701a94f5a48e5fd4f56b9b78059c32
Cristian David Salcedo <cristian.salcedo@outlook.com>
2023-06-22 21:25:02 -0300

    FinalProyecto12

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import { userContext } from "../contexts/userContext";
 function Card({card, name, link, likes, handleCardClick, onCardLike, onCardDelete}) {
   const currentUser = useContext(userContext);
   const isOwn = card.owner._id === currentUser._id;
-  const cardDeleteButtonClassName = `${isOwn ? "button-trash" : ""}`;
+  const cardDeleteButtonClassName = `button-trash ${isOwn ? "button-trash_visible" : "button-trash_hidden"}`;
   const isLiked = card.likes.some((item) => item._id === currentUser._id);
   const cardLikeButtonClassName = `button-like ${
     isLiked ? "button-like-active" : ""
@@ -21,14 +21,13 @@ function Card({card, name, link, likes, handleCardClick, onCardLike, onCardDelet
   return (
     <div className="template__card">
       <div className="cards__item">
-        <button type="button" className={cardDeleteButtonClassName} onClick={handleCardDelete}></button>
         <img 
           onClick={() => handleCardClick({name, link})}
           src={link}
           alt=""
           className="cards__image"
         />
-        {/* <button type="button" className={cardDeleteButtonClassName} onClick={handleCardDelete}></button> */}
+        <button className={cardDeleteButtonClassName} onClick={handleCardDelete}></button>
         <div className="cards__text">
             <h4 className="cards__name">{name}</h4>
             <div className="cards__like">
